0b47c6c0a3be75224cf16765f164edbd544d90c5
rahul <rgrahulgautam3@gmail.com>
2019-11-12 17:48:40 +0530

    New changes done with error, will configure new framework

--- src/main/java/assist/BrowserHelper.java
+++ src/main/java/assist/BrowserHelper.java
@@ -1,9 +1,14 @@
 package main.java.assist;
 
 import com.relevantcodes.extentreports.ExtentTest;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+
+import java.io.File;
 
 public class BrowserHelper {
     public WebDriver driver=null;
@@ -13,7 +18,10 @@ public class BrowserHelper {
         switch(browserName){
 
             case "chrome"   :   System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//main//resources//chromedriver.exe");
-                                driver=new ChromeDriver();
+                                ChromeOptions options=new ChromeOptions();
+                                options.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+                                options.addExtensions(new File(System.getProperty("user.dir")+"//src//main//resources//symatec.crx"));
+                                driver=new ChromeDriver(options);
 
                                 break;
 
